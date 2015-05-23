b09ae38e14657886a8fc2fe43c65a239e1856af6
Peter <marc.haubenstock@outlook.com>
2015-05-23 20:06:27 +0200

    server now can be queried and saves data submissions

--- nonUnity/applications/HealthSdkNancyServer/HealthSdkNancyServer/IndexModule.cs
+++ nonUnity/applications/HealthSdkNancyServer/HealthSdkNancyServer/IndexModule.cs
@@ -1,13 +1,91 @@
-﻿namespace HealthSdkNancyServer
-{
-    using Nancy;
+﻿using System.Collections.Generic;
+using System.IO;
+using Nancy;
+using Nancy.ModelBinding;
+using Newtonsoft.Json;
 
+namespace HealthSdkNancyServer
+{
     public class IndexModule : NancyModule
     {
+        private const string FILE_NAME = "eventHistory.json";
+
         public IndexModule()
         {
-            Get["/"] = _ => "Hello World!";
+            Get["/"] = parameters => View["index"];
+            Get["/status"] = _ => "I'm Alive!";
 
+            Post["/submitData"] = _ => ProcessSubmission();
+
+            Get["/getDataForUser/{userId}"] = parameters => GetDataForUser(parameters.userId);
         }
+
+        private dynamic GetDataForUser(int userId)
+        {
+            var eventHistory = LoadEventHistory();
+
+            if (eventHistory.ContainsKey(userId))
+                return eventHistory[userId].ToArray();
+            return string.Format("Error: UserId: {0} not found", userId);
+        }
+
+        private dynamic ProcessSubmission()
+        {
+            var submissionDto = this.Bind<SubmissionDTO>();
+
+            var eventHistory = LoadEventHistory();
+
+            if (!eventHistory.ContainsKey(submissionDto.UserId))
+            {
+                eventHistory.Add(submissionDto.UserId, new List<EventEntry>());
+            }
+
+            eventHistory[submissionDto.UserId].Add(new EventEntry
+            {
+                ApplicationId = submissionDto.ApplicationId,
+                EventId = submissionDto.EventId
+            });
+
+            SaveEventHistory(eventHistory);
+
+
+            var userEventEntries = eventHistory[submissionDto.UserId].ToArray();
+            return Negotiate
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithModel(userEventEntries);
+        }
+
+        private static void SaveEventHistory(Dictionary<int, List<EventEntry>> eventHistory)
+        {
+            var json = JsonConvert.SerializeObject(eventHistory, Formatting.Indented);
+            File.WriteAllText(FILE_NAME, json);
+        }
+
+        private static Dictionary<int, List<EventEntry>> LoadEventHistory()
+        {
+            var eventHistory = new Dictionary<int, List<EventEntry>>();
+
+            if (File.Exists(FILE_NAME))
+            {
+                var file = File.ReadAllText(FILE_NAME);
+                eventHistory =
+                    (Dictionary<int, List<EventEntry>>)
+                        JsonConvert.DeserializeObject(file, typeof (Dictionary<int, List<EventEntry>>));
+            }
+            return eventHistory;
+        }
+    }
+
+    internal class SubmissionDTO
+    {
+        public int UserId { get; set; }
+        public int ApplicationId { get; set; }
+        public int EventId { get; set; }
+    }
+
+    internal class EventEntry
+    {
+        public int ApplicationId { get; set; }
+        public int EventId { get; set; }
     }
 }
